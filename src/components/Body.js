48f94ce591e01736724ae73eb9907dc8eef9d3ad
durgaraoande <100592850+durgaraoande@users.noreply.github.com>
2024-08-11 17:36:01 +0530

    search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,56 @@
 import RestaurentCard from "./RestaurentCard";
 import { restaruentList } from "../constants";
+import React, { useState } from "react";
+
+function filterData(inputText, restaruents) {
+  const res= restaruents.filter((restaruent) =>
+    restaruent.info.name.includes(inputText)
+  );
+  return res;
+}
 
 const Body = () => {
+  const [inputText, setInputText] = useState("");
+  const [restaruents, setRestaruent] = useState(restaruentList);
   return (
-    <div className="cardsList">
-      {restaruentList.map((restaruent) => {
-        return <RestaurentCard {...restaruent.info} key={restaruent.info.id} />;
-      })}
-      {/* <RestaurentCard {...restaruentList[1].info}/>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Enter Text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(inputText, restaruents);
+            setRestaruent(data);
+          }}
+        >
+          Search
+        </button>
+        <button
+          className="clear-btn"
+          onClick={() => {
+            setInputText("");
+            setRestaruent(restaruentList);
+          }}
+        >
+          Clear
+        </button>
+      </div>
+      <div className="cardsList">
+        {restaruents.map((restaruent) => {
+          return (
+            <RestaurentCard {...restaruent.info} key={restaruent.info.id} />
+          );
+        })}
+        {/* <RestaurentCard {...restaruentList[1].info}/>
         <RestaurentCard restaruent={restaruentList[2]}/> */}
-    </div>
+      </div>
+    </>
   );
 };
 export default Body;
